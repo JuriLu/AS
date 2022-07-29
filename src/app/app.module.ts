51f8ddd5c64a8887e3e546d27b35248f4a13c9a5
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-29 10:24:10 +0200

    Bootstrap add Angular material add App Configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './Shared/header-bar/header-bar.component';
+import { CategoriesBarComponent } from './Shared/categories-bar/categories-bar.component';
+import { BmwListComponent } from './Components/bmw-list/bmw-list.component';
+import { BmwSingleComponent } from './Components/bmw-single/bmw-single.component';
+import { NewsComponent } from './Shared/header-bar/news/news.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    CategoriesBarComponent,
+    BmwListComponent,
+    BmwSingleComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
