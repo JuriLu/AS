7a8e4b2690bd06db5096eb021cb0025e8f86cfe4
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-29 10:17:48 +0200

    New Components Added

---
+++ src/app/Components/bmw-list/bmw-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmw-list',
+  templateUrl: './bmw-list.component.html',
+  styleUrls: ['./bmw-list.component.scss']
+})
+export class BmwListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Components/bmw-single/bmw-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmw-single',
+  templateUrl: './bmw-single.component.html',
+  styleUrls: ['./bmw-single.component.scss']
+})
+export class BmwSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
