a26b3fa92dbb76de63c1a2bfaa109d6dc7601eec
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-07-31 13:29:04 +0200

    bmw model created

---
+++ src/app/Models/bmw.model.ts
@@ -0,0 +1,12 @@
+export type engComb = 'diesel'|'petrol'
+
+export interface BmwModel{
+  img:string
+  cModel: string,
+  generation: string,
+  engineCm3: number,
+  engineCombustion:engComb
+  YOP: number,                  //Year of production
+  bodyType: string,
+  power:number
+}
