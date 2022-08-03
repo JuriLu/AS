cf1cd6f9756981a636a31a943b93f42d4d116c9b
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-08-03 10:44:09 +0200

    New Header UI Bootstrap example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  category:string = ''
+  category:string = 'NewModels'
 
   onOldSchool(){
     this.category = 'OldSchool'
