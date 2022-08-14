d7abe46f02a3dbe6c88489be6a45eeac74036fca
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-08-14 16:23:58 +0200

    Service created and logic added to get the BMW list

--- src/app/services/bmw.service.ts
+++ src/app/services/bmw.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable, shareReplay, tap} from "rxjs";
+import {BmwModel} from "../Models/bmw.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BmwService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  loadBMWs(): Observable<BmwModel[]>{
+    return this.http.get<BmwModel[]>(environment.apiUrlBMWs)
+      .pipe(
+        tap(res => console.log('res',res)),
+        shareReplay()
+      )
+
+  }
 
 
 }
