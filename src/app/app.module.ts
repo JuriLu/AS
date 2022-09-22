96c0036522193c72b3d3dce227d2ff9f7d3d6f36
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-09-22 20:35:01 +0200

    New Logo, app module change for the refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {DragDropModule} from "@angular/cdk/drag-drop";
 import {MatButtonModule} from "@angular/material/button";
 import {MatDialogModule} from "@angular/material/dialog";
-import {DialogComponent} from "./Shared/header-bar/AddBMWDialog/dialog-component";
+import {DialogComponent} from "./Components/AddBMWDialog/dialog-component";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
