8dacc70717010fbc03f13ba5e164adb5255ee8e8
JurgenLubonja <jurgen.lubonja@eng.it>
2023-06-27 11:57:32 +0200

    Structure Refactor x

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {AScarsService} from "./services/AScars.service";
 import {ASnewsService} from "./services/ASnews.service";
 import {CategoryService} from "./services/category.service";
 
 
 @NgModule({
-  // declarations: [],
-  // imports: [
-  //   CommonModule
-  // ]
   providers:[
     AScarsService,
     ASnewsService,
